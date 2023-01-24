7c6b88b3bda7b5de0220cf628e1b5f18850db5e4
str_hc <fbpetrella@gmail.com>
2023-01-24 13:12:33 +0100

    .

--- client/src/utils/axios-utils.js
+++ client/src/utils/axios-utils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const clientAPI = axios.create({ baseURL: "https://evconnection.vercel.app" });
+export const clientAPI = axios.create({ baseURL: "https://evconnection.vercel.app/" });
 
 export const signup = (user) => clientAPI.post("/user/sign_up", user);
 
